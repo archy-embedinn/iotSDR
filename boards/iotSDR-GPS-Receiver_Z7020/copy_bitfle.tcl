40d9a0deee8175c27069c5f34d0730ae93d5eeae
archy <iq@embedinn.com>
2022-06-04 12:23:02 +0500

    make generic project script

--- boards/iotSDR-GPS-Receiver_Z7020/copy_bitfle.tcl
+++ boards/iotSDR-GPS-Receiver_Z7020/copy_bitfle.tcl
@@ -2,8 +2,18 @@
 # iotSDR GPS Design bitfile to git repo
 
 set origin_dir "."
-file delete -force $origin_dir/iotSDR_GPS/bitstream/iotSDR_GPS.bit
-file copy -force $origin_dir/iotSDR_GPS_pynq/iotSDR_GPS.runs/impl_1/design_1_wrapper.bit $origin_dir/iotSDR_GPS/bitstream/iotSDR_GPS.bit
-file copy -force $origin_dir/iotSDR_GPS_pynq/iotSDR_GPS.srcs/sources_1/bd/design_1/hw_handoff/design_1.hwh $origin_dir/iotSDR_GPS/bitstream/iotSDR_GPS.hwh
-file copy -force $origin_dir/iotSDR_GPS_pynq/iotSDR_GPS.runs/impl_1/design_1_wrapper.ltx $origin_dir/iotSDR_GPS/bitstream/iotSDR_GPS.ltx
+set project "iotSDR_GPS_Receiver"
+file delete -force $origin_dir/bitstream/$project.bit
+file copy -force $origin_dir/iotSDR_GPS_pynq/iotSDR_GPS.runs/impl_1/design_1_wrapper.bit $origin_dir/bitstream/$project.bit
+file copy -force $origin_dir/iotSDR_GPS_pynq/iotSDR_GPS.srcs/sources_1/bd/design_1/hw_handoff/design_1.hwh $origin_dir/bitstream/$project.hwh
 
+set code [catch {
+        file copy -force $origin_dir/iotSDR_GPS_pynq/iotSDR_GPS.runs/impl_1/design_1_wrapper.ltx $origin_dir/bitstream/$project.ltx
+
+} result]
+
+if {$code == 0} {
+    puts "Result was $result"
+} elseif {$code == 1} {
+    puts "ILA is not used in the current design"
+} 
